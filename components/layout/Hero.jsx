ca7dc501bc68f835023f2d9c94ee69848bde8a65
Samy Zogeyb <samyzogeyb@gmail.com>
2022-07-03 22:52:49 +0400

    feat: add links ot toastice

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({
   variants,
 }) => (
   <motion.section variants={variants} id={id} className="flex justify-center w-screen">
-    <div className="max-w-screen-md w-full px-4 prose-h1:text-6xl mt-60 prose-strong:text-sky-500">
+    <div className="max-w-screen-md w-full px-4 prose-h1:text-6xl mt-60 prose-strong:text-sky-500 prose-a:font-medium prose-a:text-sky-500">
       <ReactMarkdown>
         {content}
       </ReactMarkdown>
