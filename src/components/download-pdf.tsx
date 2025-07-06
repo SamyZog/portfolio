64c72c1cd4efa4cbb18939863fcadca7e6bcec4f
Samy Zogeyb <samyzogeyb@gmail.com>
2025-07-06 04:02:41 +0400

    fix esm module error

--- src/components/download-pdf.tsx
+++ src/components/download-pdf.tsx
@@ -1,22 +1,13 @@
 "use client";
 
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import { IconDownload } from "@tabler/icons-react";
-import dynamic from "next/dynamic";
 import { type PropsWithChildren } from "react";
 
 import type { Locale } from "@/i18n/config";
 
 import { Button } from "./button";
 import { Resume } from "./resume";
-import { Skeleton } from "./skeleton";
-
-const PDFDownloadLink = dynamic(
-  () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
-  {
-    ssr: false,
-    loading: () => <Skeleton className="h-7 w-32" />,
-  },
-);
 
 export const DownloadPDF = ({
   locale,
