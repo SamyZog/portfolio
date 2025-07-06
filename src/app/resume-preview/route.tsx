751bf8c8eafb0bc9ddce8528439733ac77c9a00c
Samy Zogeyb <samyzogeyb@gmail.com>
2025-07-06 14:22:26 +0400

    add static caching to route handler

--- src/app/resume-preview/route.tsx
+++ src/app/resume-preview/route.tsx
@@ -4,18 +4,12 @@ import { NextResponse } from "next/server";
 import { Resume } from "@/components/resume";
 import { getUserLocale } from "@/i18n/locale";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const dynamic = "force-static";
 
 export const GET = async () => {
   const locale = await getUserLocale();
 
   const stream = await renderToStream(<Resume locale={locale} />);
 
-  return new NextResponse(stream as unknown as ReadableStream, {
-    headers: {
-      "Content-Type": "application/pdf",
-      "Cache-Control": "no-store, max-age=0",
-    },
-  });
+  return new NextResponse(stream as unknown as ReadableStream);
 };
